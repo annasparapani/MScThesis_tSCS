f3ebd4625de4b3707e26aae968657a804a168820
Anna Sparapani <113983183+annasparapani@users.noreply.github.com>
2023-10-30 12:31:30 +0100

    Revert "order in EMG folder"
    This reverts commit ce76b10eebd86c77d6b0d5bcc895cd85f88d8c1c.

---
+++ myTMSI_ert_linux/myTMSI_types.h
@@ -0,0 +1,36 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// File: myTMSI_types.h
+//
+// Code generated for Simulink model 'myTMSI'.
+//
+// Model version                  : 1.571
+// Simulink Coder version         : 8.13 (R2017b) 24-Jul-2017
+// C/C++ source code generated on : Mon Oct 30 10:53:21 2023
+//
+// Target selection: ert_linux.tlc
+// Embedded hardware selection: 32-bit Generic
+// Code generation objectives: Unspecified
+// Validation result: Not run
+//
+#ifndef RTW_HEADER_myTMSI_types_h_
+#define RTW_HEADER_myTMSI_types_h_
+#include "rtwtypes.h"
+#include "multiword_types.h"
+
+// Parameters (auto storage)
+typedef struct Parameters_myTMSI_ Parameters_myTMSI;
+
+// Forward declaration for rtModel
+typedef struct tag_RTM_myTMSI RT_MODEL_myTMSI;
+
+#endif                                 // RTW_HEADER_myTMSI_types_h_
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
