deb6c40a492399ccd9374d6a31f0575b3a43d76d
annasparapani <anna.sparapani@mail.polimi.it>
2023-10-30 12:23:59 +0100

    Added EMG reading code

---
+++ myTMSI_AnnaSparapani_ert_linux/myTMSI_AnnaSparapani_targ_data_map.m
@@ -0,0 +1,544 @@
+  function targMap = targDataMap(),
+
+  ;%***********************
+  ;% Create Parameter Map *
+  ;%***********************
+      
+    nTotData      = 0; %add to this count as we go
+    nTotSects     = 3;
+    sectIdxOffset = 0;
+    
+    ;%
+    ;% Define dummy sections & preallocate arrays
+    ;%
+    dumSection.nData = -1;  
+    dumSection.data  = [];
+    
+    dumData.logicalSrcIdx = -1;
+    dumData.dtTransOffset = -1;
+    
+    ;%
+    ;% Init/prealloc paramMap
+    ;%
+    paramMap.nSections           = nTotSects;
+    paramMap.sectIdxOffset       = sectIdxOffset;
+      paramMap.sections(nTotSects) = dumSection; %prealloc
+    paramMap.nTotData            = -1;
+    
+    ;%
+    ;% Auto data (myTMSI_AnnaSparapani_P)
+    ;%
+      section.nData     = 55;
+      section.data(55)  = dumData; %prealloc
+      
+	  ;% myTMSI_AnnaSparapani_P.Constant10_Value
+	  section.data(1).logicalSrcIdx = 0;
+	  section.data(1).dtTransOffset = 0;
+	
+	  ;% myTMSI_AnnaSparapani_P.Constant18_Value
+	  section.data(2).logicalSrcIdx = 1;
+	  section.data(2).dtTransOffset = 1;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter4_NumCoef
+	  section.data(3).logicalSrcIdx = 2;
+	  section.data(3).dtTransOffset = 2;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter4_DenCoef
+	  section.data(4).logicalSrcIdx = 3;
+	  section.data(4).dtTransOffset = 6;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter4_InitialStates
+	  section.data(5).logicalSrcIdx = 4;
+	  section.data(5).dtTransOffset = 10;
+	
+	  ;% myTMSI_AnnaSparapani_P.Constant9_Value
+	  section.data(6).logicalSrcIdx = 5;
+	  section.data(6).dtTransOffset = 11;
+	
+	  ;% myTMSI_AnnaSparapani_P.Constant8_Value
+	  section.data(7).logicalSrcIdx = 6;
+	  section.data(7).dtTransOffset = 12;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter5_NumCoef
+	  section.data(8).logicalSrcIdx = 7;
+	  section.data(8).dtTransOffset = 13;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter5_DenCoef
+	  section.data(9).logicalSrcIdx = 8;
+	  section.data(9).dtTransOffset = 17;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter5_InitialStates
+	  section.data(10).logicalSrcIdx = 9;
+	  section.data(10).dtTransOffset = 21;
+	
+	  ;% myTMSI_AnnaSparapani_P.Constant13_Value
+	  section.data(11).logicalSrcIdx = 10;
+	  section.data(11).dtTransOffset = 22;
+	
+	  ;% myTMSI_AnnaSparapani_P.Constant16_Value
+	  section.data(12).logicalSrcIdx = 11;
+	  section.data(12).dtTransOffset = 23;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter6_NumCoef
+	  section.data(13).logicalSrcIdx = 12;
+	  section.data(13).dtTransOffset = 24;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter6_DenCoef
+	  section.data(14).logicalSrcIdx = 13;
+	  section.data(14).dtTransOffset = 28;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter6_InitialStates
+	  section.data(15).logicalSrcIdx = 14;
+	  section.data(15).dtTransOffset = 32;
+	
+	  ;% myTMSI_AnnaSparapani_P.Constant12_Value
+	  section.data(16).logicalSrcIdx = 15;
+	  section.data(16).dtTransOffset = 33;
+	
+	  ;% myTMSI_AnnaSparapani_P.Constant11_Value
+	  section.data(17).logicalSrcIdx = 16;
+	  section.data(17).dtTransOffset = 34;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter7_NumCoef
+	  section.data(18).logicalSrcIdx = 17;
+	  section.data(18).dtTransOffset = 35;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter7_DenCoef
+	  section.data(19).logicalSrcIdx = 18;
+	  section.data(19).dtTransOffset = 39;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter7_InitialStates
+	  section.data(20).logicalSrcIdx = 19;
+	  section.data(20).dtTransOffset = 43;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter2_NumCoef
+	  section.data(21).logicalSrcIdx = 20;
+	  section.data(21).dtTransOffset = 44;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter2_DenCoef
+	  section.data(22).logicalSrcIdx = 21;
+	  section.data(22).dtTransOffset = 48;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter2_InitialStates
+	  section.data(23).logicalSrcIdx = 22;
+	  section.data(23).dtTransOffset = 52;
+	
+	  ;% myTMSI_AnnaSparapani_P.Constant12_Value_e
+	  section.data(24).logicalSrcIdx = 23;
+	  section.data(24).dtTransOffset = 53;
+	
+	  ;% myTMSI_AnnaSparapani_P.Constant12_Value_o
+	  section.data(25).logicalSrcIdx = 24;
+	  section.data(25).dtTransOffset = 54;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter7_NumCoef_g
+	  section.data(26).logicalSrcIdx = 25;
+	  section.data(26).dtTransOffset = 55;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter7_DenCoef_j
+	  section.data(27).logicalSrcIdx = 26;
+	  section.data(27).dtTransOffset = 59;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter7_InitialStates_j
+	  section.data(28).logicalSrcIdx = 27;
+	  section.data(28).dtTransOffset = 63;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter3_NumCoef
+	  section.data(29).logicalSrcIdx = 28;
+	  section.data(29).dtTransOffset = 64;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter3_DenCoef
+	  section.data(30).logicalSrcIdx = 29;
+	  section.data(30).dtTransOffset = 68;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter3_InitialStates
+	  section.data(31).logicalSrcIdx = 30;
+	  section.data(31).dtTransOffset = 72;
+	
+	  ;% myTMSI_AnnaSparapani_P.Constant12_Value_i
+	  section.data(32).logicalSrcIdx = 31;
+	  section.data(32).dtTransOffset = 73;
+	
+	  ;% myTMSI_AnnaSparapani_P.Constant14_Value
+	  section.data(33).logicalSrcIdx = 32;
+	  section.data(33).dtTransOffset = 74;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter7_NumCoef_k
+	  section.data(34).logicalSrcIdx = 33;
+	  section.data(34).dtTransOffset = 75;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter7_DenCoef_h
+	  section.data(35).logicalSrcIdx = 34;
+	  section.data(35).dtTransOffset = 79;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter7_InitialStates_p
+	  section.data(36).logicalSrcIdx = 35;
+	  section.data(36).dtTransOffset = 83;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter9_NumCoef
+	  section.data(37).logicalSrcIdx = 36;
+	  section.data(37).dtTransOffset = 84;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter9_DenCoef
+	  section.data(38).logicalSrcIdx = 37;
+	  section.data(38).dtTransOffset = 88;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter9_InitialStates
+	  section.data(39).logicalSrcIdx = 38;
+	  section.data(39).dtTransOffset = 92;
+	
+	  ;% myTMSI_AnnaSparapani_P.Constant12_Value_g
+	  section.data(40).logicalSrcIdx = 39;
+	  section.data(40).dtTransOffset = 93;
+	
+	  ;% myTMSI_AnnaSparapani_P.Constant15_Value
+	  section.data(41).logicalSrcIdx = 40;
+	  section.data(41).dtTransOffset = 94;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter7_NumCoef_h
+	  section.data(42).logicalSrcIdx = 41;
+	  section.data(42).dtTransOffset = 95;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter7_DenCoef_g
+	  section.data(43).logicalSrcIdx = 42;
+	  section.data(43).dtTransOffset = 99;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter7_InitialStates_n
+	  section.data(44).logicalSrcIdx = 43;
+	  section.data(44).dtTransOffset = 103;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter7_NumCoef_n
+	  section.data(45).logicalSrcIdx = 44;
+	  section.data(45).dtTransOffset = 104;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter7_DenCoef_n
+	  section.data(46).logicalSrcIdx = 45;
+	  section.data(46).dtTransOffset = 108;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter7_InitialStates_o
+	  section.data(47).logicalSrcIdx = 46;
+	  section.data(47).dtTransOffset = 112;
+	
+	  ;% myTMSI_AnnaSparapani_P.Constant12_Value_d
+	  section.data(48).logicalSrcIdx = 47;
+	  section.data(48).dtTransOffset = 113;
+	
+	  ;% myTMSI_AnnaSparapani_P.Constant19_Value
+	  section.data(49).logicalSrcIdx = 48;
+	  section.data(49).dtTransOffset = 114;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter7_NumCoef_gx
+	  section.data(50).logicalSrcIdx = 49;
+	  section.data(50).dtTransOffset = 115;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter7_DenCoef_l
+	  section.data(51).logicalSrcIdx = 50;
+	  section.data(51).dtTransOffset = 119;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter7_InitialStates_l
+	  section.data(52).logicalSrcIdx = 51;
+	  section.data(52).dtTransOffset = 123;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter8_NumCoef
+	  section.data(53).logicalSrcIdx = 52;
+	  section.data(53).dtTransOffset = 124;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter8_DenCoef
+	  section.data(54).logicalSrcIdx = 53;
+	  section.data(54).dtTransOffset = 128;
+	
+	  ;% myTMSI_AnnaSparapani_P.DiscreteFilter8_InitialStates
+	  section.data(55).logicalSrcIdx = 54;
+	  section.data(55).dtTransOffset = 132;
+	
+      nTotData = nTotData + section.nData;
+      paramMap.sections(1) = section;
+      clear section
+      
+      section.nData     = 3;
+      section.data(3)  = dumData; %prealloc
+      
+	  ;% myTMSI_AnnaSparapani_P.TMSI_p2
+	  section.data(1).logicalSrcIdx = 55;
+	  section.data(1).dtTransOffset = 0;
+	
+	  ;% myTMSI_AnnaSparapani_P.TMSI_p3
+	  section.data(2).logicalSrcIdx = 56;
+	  section.data(2).dtTransOffset = 1;
+	
+	  ;% myTMSI_AnnaSparapani_P.TMSI_p5
+	  section.data(3).logicalSrcIdx = 57;
+	  section.data(3).dtTransOffset = 2;
+	
+      nTotData = nTotData + section.nData;
+      paramMap.sections(2) = section;
+      clear section
+      
+      section.nData     = 1;
+      section.data(1)  = dumData; %prealloc
+      
+	  ;% myTMSI_AnnaSparapani_P.TMSI_p1
+	  section.data(1).logicalSrcIdx = 58;
+	  section.data(1).dtTransOffset = 0;
+	
+      nTotData = nTotData + section.nData;
+      paramMap.sections(3) = section;
+      clear section
+      
+    
+      ;%
+      ;% Non-auto Data (parameter)
+      ;%
+    
+
+    ;%
+    ;% Add final counts to struct.
+    ;%
+    paramMap.nTotData = nTotData;
+    
+
+
+  ;%**************************
+  ;% Create Block Output Map *
+  ;%**************************
+      
+    nTotData      = 0; %add to this count as we go
+    nTotSects     = 2;
+    sectIdxOffset = 0;
+    
+    ;%
+    ;% Define dummy sections & preallocate arrays
+    ;%
+    dumSection.nData = -1;  
+    dumSection.data  = [];
+    
+    dumData.logicalSrcIdx = -1;
+    dumData.dtTransOffset = -1;
+    
+    ;%
+    ;% Init/prealloc sigMap
+    ;%
+    sigMap.nSections           = nTotSects;
+    sigMap.sectIdxOffset       = sectIdxOffset;
+      sigMap.sections(nTotSects) = dumSection; %prealloc
+    sigMap.nTotData            = -1;
+    
+    ;%
+    ;% Auto data (myTMSI_AnnaSparapani_B)
+    ;%
+      section.nData     = 9;
+      section.data(9)  = dumData; %prealloc
+      
+	  ;% myTMSI_AnnaSparapani_B.TMSI_o1
+	  section.data(1).logicalSrcIdx = 0;
+	  section.data(1).dtTransOffset = 0;
+	
+	  ;% myTMSI_AnnaSparapani_B.DiscreteFilter4
+	  section.data(2).logicalSrcIdx = 1;
+	  section.data(2).dtTransOffset = 240;
+	
+	  ;% myTMSI_AnnaSparapani_B.DiscreteFilter5
+	  section.data(3).logicalSrcIdx = 2;
+	  section.data(3).dtTransOffset = 241;
+	
+	  ;% myTMSI_AnnaSparapani_B.DiscreteFilter6
+	  section.data(4).logicalSrcIdx = 3;
+	  section.data(4).dtTransOffset = 242;
+	
+	  ;% myTMSI_AnnaSparapani_B.DiscreteFilter2
+	  section.data(5).logicalSrcIdx = 4;
+	  section.data(5).dtTransOffset = 243;
+	
+	  ;% myTMSI_AnnaSparapani_B.DiscreteFilter3
+	  section.data(6).logicalSrcIdx = 5;
+	  section.data(6).dtTransOffset = 244;
+	
+	  ;% myTMSI_AnnaSparapani_B.DiscreteFilter9
+	  section.data(7).logicalSrcIdx = 6;
+	  section.data(7).dtTransOffset = 245;
+	
+	  ;% myTMSI_AnnaSparapani_B.DiscreteFilter7
+	  section.data(8).logicalSrcIdx = 7;
+	  section.data(8).dtTransOffset = 246;
+	
+	  ;% myTMSI_AnnaSparapani_B.DiscreteFilter8
+	  section.data(9).logicalSrcIdx = 8;
+	  section.data(9).dtTransOffset = 247;
+	
+      nTotData = nTotData + section.nData;
+      sigMap.sections(1) = section;
+      clear section
+      
+      section.nData     = 1;
+      section.data(1)  = dumData; %prealloc
+      
+	  ;% myTMSI_AnnaSparapani_B.TMSI_o2
+	  section.data(1).logicalSrcIdx = 9;
+	  section.data(1).dtTransOffset = 0;
+	
+      nTotData = nTotData + section.nData;
+      sigMap.sections(2) = section;
+      clear section
+      
+    
+      ;%
+      ;% Non-auto Data (signal)
+      ;%
+    
+
+    ;%
+    ;% Add final counts to struct.
+    ;%
+    sigMap.nTotData = nTotData;
+    
+
+
+  ;%*******************
+  ;% Create DWork Map *
+  ;%*******************
+      
+    nTotData      = 0; %add to this count as we go
+    nTotSects     = 2;
+    sectIdxOffset = 2;
+    
+    ;%
+    ;% Define dummy sections & preallocate arrays
+    ;%
+    dumSection.nData = -1;  
+    dumSection.data  = [];
+    
+    dumData.logicalSrcIdx = -1;
+    dumData.dtTransOffset = -1;
+    
+    ;%
+    ;% Init/prealloc dworkMap
+    ;%
+    dworkMap.nSections           = nTotSects;
+    dworkMap.sectIdxOffset       = sectIdxOffset;
+      dworkMap.sections(nTotSects) = dumSection; %prealloc
+    dworkMap.nTotData            = -1;
+    
+    ;%
+    ;% Auto data (myTMSI_AnnaSparapani_DWork)
+    ;%
+      section.nData     = 13;
+      section.data(13)  = dumData; %prealloc
+      
+	  ;% myTMSI_AnnaSparapani_DWork.DiscreteFilter4_states
+	  section.data(1).logicalSrcIdx = 0;
+	  section.data(1).dtTransOffset = 0;
+	
+	  ;% myTMSI_AnnaSparapani_DWork.DiscreteFilter5_states
+	  section.data(2).logicalSrcIdx = 1;
+	  section.data(2).dtTransOffset = 3;
+	
+	  ;% myTMSI_AnnaSparapani_DWork.DiscreteFilter6_states
+	  section.data(3).logicalSrcIdx = 2;
+	  section.data(3).dtTransOffset = 6;
+	
+	  ;% myTMSI_AnnaSparapani_DWork.DiscreteFilter7_states
+	  section.data(4).logicalSrcIdx = 3;
+	  section.data(4).dtTransOffset = 9;
+	
+	  ;% myTMSI_AnnaSparapani_DWork.DiscreteFilter2_states
+	  section.data(5).logicalSrcIdx = 4;
+	  section.data(5).dtTransOffset = 12;
+	
+	  ;% myTMSI_AnnaSparapani_DWork.DiscreteFilter7_states_m
+	  section.data(6).logicalSrcIdx = 5;
+	  section.data(6).dtTransOffset = 15;
+	
+	  ;% myTMSI_AnnaSparapani_DWork.DiscreteFilter3_states
+	  section.data(7).logicalSrcIdx = 6;
+	  section.data(7).dtTransOffset = 18;
+	
+	  ;% myTMSI_AnnaSparapani_DWork.DiscreteFilter7_states_k
+	  section.data(8).logicalSrcIdx = 7;
+	  section.data(8).dtTransOffset = 21;
+	
+	  ;% myTMSI_AnnaSparapani_DWork.DiscreteFilter9_states
+	  section.data(9).logicalSrcIdx = 8;
+	  section.data(9).dtTransOffset = 24;
+	
+	  ;% myTMSI_AnnaSparapani_DWork.DiscreteFilter7_states_h
+	  section.data(10).logicalSrcIdx = 9;
+	  section.data(10).dtTransOffset = 27;
+	
+	  ;% myTMSI_AnnaSparapani_DWork.DiscreteFilter7_states_i
+	  section.data(11).logicalSrcIdx = 10;
+	  section.data(11).dtTransOffset = 30;
+	
+	  ;% myTMSI_AnnaSparapani_DWork.DiscreteFilter7_states_kj
+	  section.data(12).logicalSrcIdx = 11;
+	  section.data(12).dtTransOffset = 33;
+	
+	  ;% myTMSI_AnnaSparapani_DWork.DiscreteFilter8_states
+	  section.data(13).logicalSrcIdx = 12;
+	  section.data(13).dtTransOffset = 36;
+	
+      nTotData = nTotData + section.nData;
+      dworkMap.sections(1) = section;
+      clear section
+      
+      section.nData     = 6;
+      section.data(6)  = dumData; %prealloc
+      
+	  ;% myTMSI_AnnaSparapani_DWork.TMSI_PWORK
+	  section.data(1).logicalSrcIdx = 26;
+	  section.data(1).dtTransOffset = 0;
+	
+	  ;% myTMSI_AnnaSparapani_DWork.EMG1_PWORK.LoggedData
+	  section.data(2).logicalSrcIdx = 27;
+	  section.data(2).dtTransOffset = 1;
+	
+	  ;% myTMSI_AnnaSparapani_DWork.EMG3_PWORK.LoggedData
+	  section.data(3).logicalSrcIdx = 28;
+	  section.data(3).dtTransOffset = 5;
+	
+	  ;% myTMSI_AnnaSparapani_DWork.EMGcount_PWORK.LoggedData
+	  section.data(4).logicalSrcIdx = 29;
+	  section.data(4).dtTransOffset = 9;
+	
+	  ;% myTMSI_AnnaSparapani_DWork.ToWorkspaceEMG_PWORK.LoggedData
+	  section.data(5).logicalSrcIdx = 30;
+	  section.data(5).dtTransOffset = 10;
+	
+	  ;% myTMSI_AnnaSparapani_DWork.ToWorkspaceEMGcount_PWORK.LoggedData
+	  section.data(6).logicalSrcIdx = 31;
+	  section.data(6).dtTransOffset = 11;
+	
+      nTotData = nTotData + section.nData;
+      dworkMap.sections(2) = section;
+      clear section
+      
+    
+      ;%
+      ;% Non-auto Data (dwork)
+      ;%
+    
+
+    ;%
+    ;% Add final counts to struct.
+    ;%
+    dworkMap.nTotData = nTotData;
+    
+
+
+  ;%
+  ;% Add individual maps to base struct.
+  ;%
+
+  targMap.paramMap  = paramMap;    
+  targMap.signalMap = sigMap;
+  targMap.dworkMap  = dworkMap;
+  
+  ;%
+  ;% Add checksums to base struct.
+  ;%
+
+
+  targMap.checksum0 = 244061125;
+  targMap.checksum1 = 4108008142;
+  targMap.checksum2 = 1161627707;
+  targMap.checksum3 = 3634180113;
+
---
+++ myTMSI_ert_linux/ert_main.cpp
@@ -0,0 +1,275 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// File: ert_main.cpp
+//
+// Code generated for Simulink model 'myTMSI'.
+//
+// Model version                  : 1.571
+// Simulink Coder version         : 8.13 (R2017b) 24-Jul-2017
+// C/C++ source code generated on : Mon Oct 30 10:53:21 2023
+//
+// Target selection: ert_linux.tlc
+// Embedded hardware selection: 32-bit Generic
+// Code generation objectives: Unspecified
+// Validation result: Not run
+//
+
+// Multirate - Multitasking case main file
+#define _BSD_SOURCE                                              // For usleep() 
+#define _POSIX_C_SOURCE                200112L                   // For clock_gettime() & clock_nanosleep() 
+#include <stdio.h>                     // This ert_main.c example uses printf/fflush 
+#include <pthread.h>                   // Thread library header file
+#include <sched.h>                     // OS scheduler header file
+#include <semaphore.h>                 // Semaphores library header file
+#include <time.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/mman.h>                  // For mlockall()
+#include <signal.h>
+#include "myTMSI.h"                    // Model's header file
+#include "rtwtypes.h"                  // MathWorks types
+#include "ext_work.h"                  // External mode header file
+#ifndef TRUE
+#define TRUE                           true
+#define FALSE                          false
+#endif
+
+//==================*
+//  Required defines *
+// ==================
+#ifndef MODEL
+# error Must specify a model name. Define MODEL=name.
+#else
+
+// create generic macros that work with any model
+# define EXPAND_CONCAT(name1,name2)    name1 ## name2
+# define CONCAT(name1,name2)           EXPAND_CONCAT(name1,name2)
+# define MODEL_INITIALIZE              CONCAT(MODEL,_initialize)
+# define MODEL_STEP                    CONCAT(MODEL,_step)
+# define MODEL_TERMINATE               CONCAT(MODEL,_terminate)
+# define RT_MDL                        CONCAT(MODEL,_M)
+#endif
+
+// Error checking
+#define STRINGIZE(num)                 #num
+#define POS(line)                      __FILE__ ":" STRINGIZE(line)
+#define CHECK0(expr)                   do { int __err = (expr); if (__err) { fprintf(stderr, "Error: %s returned '%s' at " POS(__LINE__) "\n", #expr, strerror(__err)); exit(1); } } while (0);
+#define CHECKE(expr)                   do { if ((expr) == -1) { perror(#expr " at " POS(__LINE__)); exit(1); } } while (0);
+
+//*
+//  Maximal priority used by base rate thread.
+
+#define MAX_PRIO                       (sched_get_priority_min(SCHED_FIFO) + 1)
+
+struct timingStats_t{
+  uint32_T overruns;
+  float meanSampleTime;
+  uint32_T numElapsedSamples;
+  float lastElapsedTime;
+  float spareTime;
+  float fundamentelSampleTime;
+};
+
+struct timingStats_t timing;
+
+// Signal handler for ABORT during simulation
+void abortHandler(int sig)
+{
+  fprintf(stderr, "Simulation aborted by pressing CTRL+C\n");
+  rtmSetStopRequested(myTMSI_M, 1);
+}
+
+//*
+//  Thread handle of the base rate thread.
+//  Fundamental sample time = 0.01s
+
+pthread_t base_rate_thread;
+
+//*
+//  Thread handles of and semaphores for sub rate threads. The array
+//  is indexed by TID, i.e. the first one or two elements are unused.
+
+struct sub_rate {
+  pthread_t thread;
+  sem_t sem;
+} sub_rate[1];
+
+//*
+//  Flag if the simulation has been terminated.
+
+int simulationFinished = 0;
+
+// Indication that the base rate thread has started
+sem_t ext_mode_ready;
+
+//*
+//  This is the thread function of the base rate loop.
+//  Fundamental sample time = 0.01s
+
+#ifdef __cplusplus
+
+void * base_rate(void * )
+#else
+  void * base_rate(void * )
+#endif
+{
+  struct timespec now, next, last_now;
+  struct timespec period = { 0U, 10000000U };// 0.01 seconds
+
+  boolean_T eventFlags[1];             // Model has 1 rates
+  int_T taskCounter[1] = { 0 };
+
+  int_T OverrunFlags[1];
+  int step_sem_value;
+  int_T i;
+
+  // assign timing stats
+  timing.fundamentelSampleTime = 0.01F;
+  timing.meanSampleTime = 0.01F;
+
+  // External mode
+  rtSetTFinalForExtMode(&rtmGetTFinal(myTMSI_M));
+  rtExtModeCheckInit(1);
+
+  {
+    boolean_T rtmStopReq = false;
+    rtExtModeWaitForStartPkt(myTMSI_M->extModeInfo, 1, &rtmStopReq);
+    if (rtmStopReq) {
+      rtmSetStopRequested(myTMSI_M, true);
+    }
+  }
+
+  rtERTExtModeStartMsg();
+  CHECKE(sem_post(&ext_mode_ready));
+  clock_gettime(CLOCK_MONOTONIC, &next);
+  last_now = next;
+
+  // Main loop, running until all the threads are terminated
+  while (rtmGetErrorStatus(myTMSI_M) == NULL && !rtmGetStopRequested(myTMSI_M))
+  {
+    // Check subrate overrun, set rates that need to run this time step
+
+    // Trigger sub-rate threads
+
+    // Execute base rate step
+    myTMSI_step();
+    rtExtModeCheckEndTrigger();
+    do {
+      next.tv_sec += period.tv_sec;
+      next.tv_nsec += period.tv_nsec;
+      if (next.tv_nsec >= 1000000000) {
+        next.tv_sec++;
+        next.tv_nsec -= 1000000000;
+      }
+
+      clock_gettime(CLOCK_MONOTONIC, &now);
+
+      // update timing statistics
+      timing.numElapsedSamples++;
+      timing.lastElapsedTime = (now.tv_sec - last_now.tv_sec) +
+        (now.tv_nsec - last_now.tv_nsec) / 1000000000.0F;
+      timing.meanSampleTime = 0.99F * timing.meanSampleTime + 0.01F *
+        timing.lastElapsedTime;
+      timing.spareTime = (next.tv_sec - now.tv_sec) +
+        (next.tv_nsec - now.tv_nsec) / 1000000000.0F;
+      last_now = now;
+
+      //fprintf(stderr, "elapsedTime = %f s, meanSampleTime = %f\n", timing.lastElapsedTime, timing.meanSampleTime);
+      if (now.tv_sec > next.tv_sec ||
+          (now.tv_sec == next.tv_sec && now.tv_nsec > next.tv_nsec)) {
+        uint32_T usec = (now.tv_sec - next.tv_sec) * 1000000 + (now.tv_nsec -
+          next.tv_nsec)/1000;
+        fprintf(stderr, "Base rate (0.01s) overrun by %d us\n", usec);
+        timing.overruns++;
+        next = now;
+        continue;
+      }
+    } while (0);
+
+    clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &next, NULL);
+  }
+
+  simulationFinished = 1;
+
+  // Final step
+  for (i = 1; i < 1; i++) {
+    sem_post(&sub_rate[i].sem);
+    sem_post(&sub_rate[i].sem);
+  }
+
+  return 0;
+}
+
+//*
+//  This is the main function of the model.
+//  Multirate - Multitasking case main file
+
+int_T main(int_T argc, const char_T *argv[])
+{
+  const char_T *errStatus;
+  int_T i;
+  pthread_attr_t attr;
+  struct sched_param sched_param;
+
+  // External mode
+  // rtERTExtModeParseArgs(argc, argv);
+  rtExtModeParseArgs(argc, argv, NULL);
+  CHECKE(sem_init(&ext_mode_ready, 0, 0));
+  CHECKE(mlockall(MCL_FUTURE));
+
+  // Initialize model
+  myTMSI_initialize();
+  simulationFinished = 0;
+
+  // Prepare task attributes
+  CHECK0(pthread_attr_init(&attr));
+  CHECK0(pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED));
+  CHECK0(pthread_attr_setschedpolicy(&attr, SCHED_FIFO));
+
+  // Starting the base rate thread
+  sched_param.sched_priority = MAX_PRIO;
+  CHECK0(pthread_attr_setschedparam(&attr, &sched_param));
+  CHECK0(pthread_create(&base_rate_thread, &attr, base_rate, NULL));
+  CHECK0(pthread_attr_destroy(&attr));
+
+  // External mode
+  CHECKE(sem_wait(&ext_mode_ready));
+  signal(SIGINT, abortHandler);        // important for letting the destructor be called.
+  while (rtmGetErrorStatus(myTMSI_M) == NULL && !rtmGetStopRequested(myTMSI_M))
+  {
+    rtExtModeOneStep(rtmGetRTWExtModeInfo(RT_MDL), NUMST, (boolean_T *)
+                     &rtmGetStopRequested(RT_MDL));
+    usleep(10000U);
+  }
+
+  // Wait for threads to finish
+  pthread_join(base_rate_thread, NULL);
+  rtExtModeShutdown(1);
+
+  // Terminate model
+  myTMSI_terminate();
+  errStatus = rtmGetErrorStatus(myTMSI_M);
+  if (errStatus != NULL && strcmp(errStatus, "Simulation finished")) {
+    if (!strcmp(errStatus, "Overrun")) {
+      printf("ISR overrun - sampling rate too fast\n");
+    }
+
+    return(1);
+  }
+
+  return 0;
+}
+
+// Local Variables:
+// compile-command: "make -f myTMSI.mk"
+// End:
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
